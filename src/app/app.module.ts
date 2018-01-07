a25b61916184fb62a40c8c4ad237e271109790fc
Aleksandr Aleksandrov <Aliaksandr_Aliaksandrau1@epam.com>
2018-01-08 00:56:35 +0300

    [fix](strange-app) Delete unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,7 @@ import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 import { APP_RESOLVER_PROVIDERS } from './app.resolver';
 import { AppState, InternalStateType } from './app.service';
-import { HomeComponent } from './home';
-import { AboutComponent } from './about';
 import { NoContentComponent } from './no-content';
-import { XLargeDirective } from './home/x-large';
-import { DevModuleModule } from './+dev-module';
 
 import '../styles/styles.scss';
 import '../styles/headings.css';
@@ -40,14 +36,11 @@ type StoreType = {
  * `AppModule` is the main entry point into Angular2's bootstraping process
  */
 @NgModule({
-  bootstrap: [ AppComponent ],
+  bootstrap: [AppComponent],
   declarations: [
     AppComponent,
-    AboutComponent,
-    HomeComponent,
-    NoContentComponent,
-    XLargeDirective
-    ],
+    NoContentComponent
+  ],
   /**
    * Import Angular's modules.
    */
@@ -60,14 +53,7 @@ type StoreType = {
     RouterModule.forRoot(ROUTES, {
       useHash: Boolean(history.pushState) === false,
       preloadingStrategy: PreloadAllModules
-    }),
-
-    /**
-     * This section will import the `DevModuleModule` only in certain build types.
-     * When the module is not imported it will get tree shaked.
-     * This is a simple example, a big app should probably implement some logic
-     */
-    ...environment.showDevModule ? [ DevModuleModule ] : [],
+    })
   ],
   /**
    * Expose our Services and Providers into Angular's dependency injection.
@@ -75,6 +61,6 @@ type StoreType = {
   providers: [
     environment.ENV_PROVIDERS,
     APP_PROVIDERS
-    ]
+  ]
 })
-export class AppModule {}
+export class AppModule { }
