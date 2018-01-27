ee77ec11a3d2a43bcfcb48c45ea31e6b0eef1d1d
Aleksandr Aleksandrov <Aliaksandr_Aliaksandrau1@epam.com>
2018-01-28 00:49:24 +0300

    [feat](finance-chart) Add bar chart

--- src/app/finance-chart/finance-chart.component.ts
+++ src/app/finance-chart/finance-chart.component.ts
@@ -1,25 +1,51 @@
 import {
     Component,
-    OnInit
+    OnInit,
+    ViewEncapsulation
 } from '@angular/core';
 import { FinanceChartService } from 'app/finance-chart/finance-chart.service';
+import * as d3 from 'd3';
+import { type } from 'os';
+import { Subject } from 'rxjs/Subject';
+import { ItemCost } from 'app/finance-chart/model/ItemCost';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Component({
     selector: 'ff-finance-chart',
     templateUrl: './finance-chart.component.html',
-    styleUrls: ['./finance-chart.component.scss']
+    styleUrls: ['./finance-chart.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class FinanceChartComponent implements OnInit {
 
+    barChartObservable: Observable<ItemCost[]>;
     public value: any;
+
+    private barChartData: ItemCost[];
+
     constructor(private financeChartService: FinanceChartService) { }
 
-    public ngOnInit(): void {
+    ngOnInit(): void {
         this.financeChartService
-        .getData()
-        .subscribe(val => {
-            this.value = val['value'];
-            console.log('val: ', val);
-        });
+            .getData()
+            .subscribe(val => {
+                this.value = val['value'];
+                console.log('val: ', val);
+            });
+
+        this.createBarChartData();
+    }
+
+    createBarChartData() {
+        let barChartData: ItemCost[] = [];
+
+        for (let i = 0; i < 12; i++) {
+            let item = new ItemCost();
+            item.date = `Month ${i + 1}`;
+            item.value = Math.floor(Math.random() * 100);
+            barChartData.push(item);
+        }
+        this.barChartObservable = Observable.of(barChartData);
     }
 }
---
+++ src/app/finance-chart/model/ItemCost.ts
@@ -0,0 +1,8 @@
+export class ItemCost {
+    public value: number;
+    public currency: string;
+    public item: string;
+    public category: string;
+    public date: Date | string;
+    public comment: string;
+}
